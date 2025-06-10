51749e203e70dbdd1c6dd64cdd1b0bec58048433
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2025-06-10 16:12:24 -0400

    Feat: added animation to navbar link

--- journeychronicles/src/components/SidebarNav.tsx
+++ journeychronicles/src/components/SidebarNav.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { motion } from "motion/react";
+
 import {
   Aside,
   IconWrapper,
@@ -18,18 +20,40 @@ type NavBarProps = {
   logo?: React.ReactNode;
 };
 
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
 const SidebarNav: React.FC<NavBarProps> = ({ items, logo }) => {
   return (
     <Aside>
       <LogoContainer>{logo}</LogoContainer>
       <NavList>
         {items.map(({ label, href, icon }) => (
-          <li key={label}>
+          <motion.li
+            key={label}
+            variants={itemVariants}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+          >
             <NavLink to={href}>
               {icon && <IconWrapper>{icon}</IconWrapper>}
               {label}
             </NavLink>
-          </li>
+          </motion.li>
         ))}
       </NavList>
     </Aside>
