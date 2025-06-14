4e726bf9ed52d3a6ec002ce13074f8acdf330a64
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2025-06-13 21:49:56 -0400

    chore: asset added

--- journeychronicles/src/App.tsx
+++ journeychronicles/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./components/Header"; // or Layout
+import Layout from "./components/Layout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import AboutPage from "./components/AboutPage";
