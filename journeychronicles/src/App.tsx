d3129f21f40fbb2733beb70b727044e1e254dea2
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2025-07-13 17:40:14 -0400

    fix: redirect handler for refreshing

--- journeychronicles/src/App.tsx
+++ journeychronicles/src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "./components/Layout";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 
 import AboutPage from "./components/AboutPage";
-import HompePage from "./components/HomePage";
+import HomePage from "./components/HomePage";
 import DestinationsPage from "./components/DestinationsPage";
 import ContactPage from "./components/ContactPage";
 import MapPage from "./components/MapPage";
@@ -12,13 +18,30 @@ import TravelTipsPage from "./components/TravelTips";
 import ScrollToTop from "./utils/scrollToTop";
 import PostPage from "./components/PostPage";
 
+function RedirectHandler() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const redirect = params.get("redirect");
+
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [location, navigate]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router basename="/Journey_Chronicles">
       <Layout>
         <ScrollToTop />
+        <RedirectHandler />
         <Routes>
-          <Route path="/" element={<HompePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/post/:id" element={<PostPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/destinations" element={<DestinationsPage />} />
